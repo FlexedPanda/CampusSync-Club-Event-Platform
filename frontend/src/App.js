50f64941ec3d94c6e8a02db8f466890c5d3eb9b4
FlexedPanda <77376571+FlexedPanda@users.noreply.github.com>
2024-11-27 22:05:14 +0600

    Updated Project Folder Structure

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
