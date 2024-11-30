ac5b687df0825b761a5a4827a27315e60300ade4
FlexedPanda <77376571+FlexedPanda@users.noreply.github.com>
2024-11-30 18:03:49 +0600

    .

--- backend/src/server.js
+++
@@ -1,9 +0,0 @@
-import app from "./app.js";
-import { connectDB } from "./config/db.js";
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-	connectDB();
-	console.log(`Server started at http://localhost:${PORT}`);
-});
