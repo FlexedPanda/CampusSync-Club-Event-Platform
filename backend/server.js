9dc2d7325521dbb82c4b9daae2c41bd13c8677d8
FlexedPanda <77376571+FlexedPanda@users.noreply.github.com>
2024-12-06 20:10:48 +0600

    .

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json());
 
 app.use("/", (req, res) => {
 	res.send("Backend API is running successfully");
